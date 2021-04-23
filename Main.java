91e95bffe9493fe0af19873e66e1c6b8f0d25a19
600020344 <77069279+600020344@users.noreply.github.com>
2021-04-23 03:07:35 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,5 +7,17 @@
 class Main {
   public static void main(String[] args) {
      //add test cases  
+     PalinList p = new PalinList("one two three two one");
+     System.out.println(p);
+     p.setList("1 2 3 4 5 one two three four five");
+     System.out.println(p);
+     p.setList("a b c d e f g x y z g f h");
+     System.out.println(p);
+     p.setList("racecar is racecar");
+     System.out.println(p);
+     p.setList("1 2 3 a b c c b a 3 2 1");
+     System.out.println(p);
+     p.setList("chicken is a chicken");
+     System.out.println(p);
   }
 }
