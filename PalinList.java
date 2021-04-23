26a62cf5c9e8167e7902369d9c4407764263eb48
600020344 <77069279+600020344@users.noreply.github.com>
2021-04-23 03:08:34 -0700

    Update PalinList.java

--- PalinList.java
+++ PalinList.java
@@ -22,17 +22,49 @@ public class PalinList
 
  public PalinList(String list)
  {
+   setList(list);
  }
 
  public void setList(String list)
  {
+   stack = new Stack<String>();
+   queue = new LinkedList<String>();
+   String[] s = list.split(" ");
+   if (list.length()!=0)
+   {
+    for (String str: s)
+    {
+      stack.push(str);
+      queue.add(str);
+    }
+   }
  }
 
  public boolean isPalin()
  {
-  return true;
+   boolean b = true;
+   Stack<String> temp = new Stack<String>();
+   while(!queue.isEmpty())
+   {
+     temp.push(stack.peek());
+     if(!queue.remove().equals(stack.pop()))
+      b = false;
+   }
+  stack = temp;
+  return b;
  }
 
 
  //write a toString method
+ public String toString()
+ {
+   String stackStr = stack.toString();
+   String result;
+   if (isPalin())
+    result = "is a palinlist.";
+  else
+    result = "is not a palinlist.";
+    return stackStr + " " + result;
+
+ }
 }
